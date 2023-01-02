26a05d2ab491bb4c6b22d6b7edca0dbe4ddf8079
sarakabocha <sarakimiam@gmail.com>
2023-01-01 20:28:03 -0800

    remove a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
 
+    for (let i = 0; i < array.length; i++) {// Loop through each object in array
+        if (arg === array[i]) { // Find matching values
+            array.splice(i, 1); // Remove match from array
+        }
+    } return array; // Return updated array
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+removeFromArray([1, 2, 3, 4], 3);
